1a69de5719c182cf8670282e6301414812b7bf79
Jacob McDonald <jacob.mcdonald@wsu.edu>
2018-04-24 18:41:31 -0700

    fix

--- Bullet.h
+++ Bullet.h
@@ -5,7 +5,7 @@ class Bullet : public sf::Sprite
 {
 public:
 	Bullet(const sf::Texture &texture, const sf::IntRect &rectangle = sf::IntRect(10,10,32,32), const sf::Vector2f pos = sf::Vector2f(0, 0)):
-		Sprite(texture, rectangle)
+		Sprite(texture)
 	{
 		this->setPosition(pos);
 	}
